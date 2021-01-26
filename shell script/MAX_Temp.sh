184179424e1a56c51759304b804e114b79fac5ab
Mohamed Adel Hassan <58120325+MohamedAdelHsn@users.noreply.github.com>
2021-01-26 03:20:30 +0200

    Update MAX_Temp.sh

--- shell script/MAX_Temp.sh
+++ shell script/MAX_Temp.sh
@@ -4,7 +4,8 @@
 #-----------------------------------------------------
 # first put or copy your dataset from local into hdfs
 hadoop fs -put /home/hdpuser/Desktop/dataset/sample.txt hdfs://localhost:9000/Test
-# run your own mapreduce job by passing jar file into hadoop with the following schema >> hadoop jar </path_of_the_jar_file>  <jarclass_name> <input_data> <output_data>
+# run your own mapreduce job by passing jar file into hadoop with the following schema :-
+# >> hadoop jar </path_of_the_jar_file>  <jarclass_name> <input_data> <output_data>
 hadoop jar '/home/hdpuser/Desktop/jars/Max_Temp.jar' com.mr.weatherAnalysis.Max_Temp /Test/sample.txt /output
 # print the output after processing 
 hadoop fs -cat /output/part-r-00000
